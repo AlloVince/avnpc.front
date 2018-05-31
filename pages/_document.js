bf8b93a0a065743f3a5cd5289f17aa8a5d3192fc
AlloVince <allo.vince@gmail.com>
2018-05-31 23:56:56 +0800

    fix: add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,21 +3,28 @@ import htmlescape from 'htmlescape';
 import config from '../universal.config';
 
 export default class Document extends Document_ {
-  static async getInitialProps (ctx) {
+  static async getInitialProps(ctx) {
     const props = await Document_.getInitialProps(ctx);
     return props;
   }
 
-  render () {
+  render() {
     return (
       <html lang="en">
-        <Head />
+        <Head/>
         <body>
-          <Main />
+          <Main/>
           <script
             dangerouslySetInnerHTML={{ __html: `process = {}; window.__ENV__ = process.env = ${htmlescape(config)}` }}
           />
-          <NextScript />
+          <NextScript/>
+          {
+            process.env.NODE_ENV === 'production' &&
+            <div>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-31189399-1"/>
+              <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-31189399-1');` }} />
+            </div>
+          }
         </body>
       </html>
     );
